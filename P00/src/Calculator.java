2284c7862b46025c26e62b605bf7fe9bcf16b10d
lihuaaaa <lihuaaaa@LAPTOP-7O1BSRUV>
2023-05-26 12:51:15 +0800

    first push to P00 repository in GitHbu

---
+++ P00/src/Calculator.java
@@ -0,0 +1,19 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * lihuaaaa, 26 May 2023 11:52:56 am
+ */
+
+public class Calculator {
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	public int subtract(int a, int b)
+	{
+		return a - b;
+	}
+}
---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * lihuaaaa, 26 May 2023 11:54:21 am
+ */
+
+public class CalculatorTest {
+	public void testAdd()
+	{
+		int a = 1234;
+		int b = 5678;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 9999;
+		assertEquals(expected,actual);
+	}
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
